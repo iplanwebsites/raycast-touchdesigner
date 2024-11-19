85db6355d1f0a38a2dc4b3ec7b89595722d40726
felix <you@example.com>
2024-11-19 10:56:43 -0500

    as

--- touchdesigner/src/tdOperators.d.ts
+++ touchdesigner/src/tdOperators.d.ts
@@ -1,19 +1,19 @@
-type Operator = {
+export type Operator = {
     name: string;
     description: string;
     sweet?: boolean;
     category?: string;
-  };
-  
-  type OperatorData = {
-    operators: {
-      [key: string]: {
-        description: string;
-        operators: Operator[];
-      };
-    };
-  };
+};
 
-  declare module 'tdOperators' {
+export type OperatorData = {
+    operators: {
+        [key: string]: {
+            description: string;
+            operators: Operator[];
+        };
+    };
+};
+
+declare module 'tdOperators' {
     export const operatorData: OperatorData;
-  }
+}
